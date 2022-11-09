68e70e7deeffba76ce72e86a90cee376524f617a
happyHHyu <117804376+happyHHyu@users.noreply.github.com>
2022-11-09 14:59:20 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,15 @@
 input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 9; index++) {
+        start_time = input.runningTime()
         start += 1
         basic.showNumber(start)
-        basic.pause(1000)
+        basic.pause(500)
     }
     start = 0
 })
+let start_time = 0
 let start = 0
 start = 0
+basic.forever(function () {
+	
+})
